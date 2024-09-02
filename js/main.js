61e7b361c78be012647caefb859f8210e6565310
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-09-02 16:01:00 +0200

    Mi sembrava di aver creato tutto correttamente solo che non e funzionante e non riesco a trovare l'errore HELP ME

--- js/main.js
+++ js/main.js
@@ -2,22 +2,22 @@ const { createApp } = Vue;
 const app = createApp({
     data () {
         return {
-            todo:[
-            " Visitare l'Argentina ",
-            " Visitare il Brasile ",
-            " Visitare la Germania",
-            " Non visitare la francia ",
-            " Non visitare la Svezia",
+            todos:[
+            ' Visitare Argentina ',
+            ' Visitare il Brasile ',
+            ' Visitare la Germania',
+            ' Non visitare la francia ',
+            ' Non visitare la Svezia',
             ],
-            newTodo: ""
+            newTodo: ''
         };
     },
     methods: {
         addTodo(){
             const cleanTodo = this.newTodo.trim();
-            if (cleanTodo != "") {
-                this.todo.push(cleanTodo);
-                this.newTodo = "";
+            if (cleanTodo != '') {
+                this.todos.push(cleanTodo);
+                this.newTodo = '';
             }
         },
         removeTodo(i) {
