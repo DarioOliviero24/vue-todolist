38040a6d7fd57155532dc0b851c465f411f4a5e4
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-09-02 15:28:52 +0200

    Aggiunti componenti html+Js-vue

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-console.log('Ciao');
+const { createApp } = Vue;
+const app = createApp({
+    data () {
+        return {
+            todo:[
+            " Visitare l'Argentina ",
+            " Visitare il Brasile ",
+            " Visitare la Germania",
+            " Non visitare la francia ",
+            " Non visitare la Svezia",
+            ],
+            newTodo: ""
+        };
+    },
+    methods: {
+        addTodo(){
+            const cleanTodo = this.newTodo.trim();
+            if (cleanTodo != "") {
+                this.todo.push(cleanTodo);
+                this.newTodo = "";
+            }
+        },
+        removeTodo(i) {
+            this.todos.slice(i, 1);
+        }
+    }
+}).mount('#app');
